402eb1cb684e3ec2f26331a71dd7bec318a4ba8c
Nina Vincent <>
2018-11-15 19:32:22 -0800

    Adding initial .py file, data csv files, helpful cheat sheets

---
+++ Project.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import numpy
+import openpyxl
+import cmath
+import math
+
+filebase = "/Users/ninavincent/Desktop/Project454.xlsx"
+
+test=openpyxl.load_workbook(filebase)
+test2 = test
+
+test[3]
+
+
+#N = 2
+#Matrix = [[0.] * N for i in range(N)]
