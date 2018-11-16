a91fb9b2a3005dabfb2904e89fb3d2a538adae48
Nina Vincent <>
2018-11-16 15:53:43 -0800

    Added function areas to work in, started setting up the read inputs naming system

--- Project.py
+++ Project.py
@@ -1,16 +1,64 @@
-# -*- coding: utf-8 -*-
 import numpy
 import openpyxl
 import cmath
 import math
+import os
 
-filebase = "/Users/ninavincent/Desktop/Project454.xlsx"
+########################
+# Input File
+########################
 
-test=openpyxl.load_workbook(filebase)
-test2 = test
 
-test[3]
+scenarioName = "1"
+
+#basefolder = "/Users/ninavincent/Desktop/Git/project454/"
+basefolder = os.getcwd()
+database = basefolder + "/data/"
+lineCSV = database + "LineData" + scenarioName + ".csv"
+busCSV = database + "BusData" + scenarioName + ".csv"
+
+
+########################
+# Constants
+########################
+
+Eps = 0.001
+MVAbase = 100
+#something else = 20 but can't read my notes
 
 
 #N = 2
 #Matrix = [[0.] * N for i in range(N)]
+
+
+###################################
+# Functions about read/write files
+##################################
+
+
+
+
+
+
+###################################
+# Functions about creating Y matrix
+##################################
+
+
+
+
+###################################
+# Functions about forming power system equations
+##################################
+
+
+
+
+###################################
+# Functions about Newton-Raphson
+##################################
+
+
+
+# Function that computes Jacobian
+
